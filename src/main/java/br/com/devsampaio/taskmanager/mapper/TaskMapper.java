707384a274c26b92c57874487591de05205d79e2
Fernanda Carolina Sampaio <dev.sampaio7@proton.me>
2026-08-13 15:14:10 -0300

    Criada a estrutura do mapper

--- src/main/java/br/com/devsampaio/taskmanager/mapper/TaskMapper.java
+++ src/main/java/br/com/devsampaio/taskmanager/mapper/TaskMapper.java
@@ -1,4 +1,19 @@
 package br.com.devsampaio.taskmanager.mapper;
 
+import br.com.devsampaio.taskmanager.dto.TaskRequestDto;
+import br.com.devsampaio.taskmanager.dto.TaskResponseDto;
+import br.com.devsampaio.taskmanager.model.Task;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface TaskMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "finishedAt", ignore = true)
+    Task toEntity(TaskRequestDto dto);
+
+
+    TaskResponseDto toDto(Task task);
 }
