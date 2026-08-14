1a1f9f3c1eccbeec9181cafa55e62ff63fd8891c
Fernanda Carolina Sampaio <dev.sampaio7@proton.me>
2026-08-14 20:10:44 -0300

    Criado o endpoint delete no controller

--- src/main/java/br/com/devsampaio/taskmanager/controller/TaskController.java
+++ src/main/java/br/com/devsampaio/taskmanager/controller/TaskController.java
@@ -45,5 +45,11 @@ public class TaskController {
         return ResponseEntity.status((HttpStatus.OK)).body(service.partialUpdate(id, dto));
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete( @PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 
 }
