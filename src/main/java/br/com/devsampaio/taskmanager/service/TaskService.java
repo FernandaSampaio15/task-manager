40383af6a707084266a8da42c6244af5cf013866
Fernanda Carolina Sampaio <dev.sampaio7@proton.me>
2026-08-14 11:46:14 -0300

    Criado exceção personalizada TaskNotFoundException e método privado searchForId no service

--- src/main/java/br/com/devsampaio/taskmanager/service/TaskService.java
+++ src/main/java/br/com/devsampaio/taskmanager/service/TaskService.java
@@ -1,10 +1,10 @@
 package br.com.devsampaio.taskmanager.service;
 
 import br.com.devsampaio.taskmanager.dto.TaskResponseDto;
+import br.com.devsampaio.taskmanager.exceptions.TaskNotFoundException;
 import br.com.devsampaio.taskmanager.mapper.TaskMapper;
 import br.com.devsampaio.taskmanager.model.Task;
 import br.com.devsampaio.taskmanager.repository.TaskRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,5 +22,12 @@ public class TaskService {
     public List<TaskResponseDto> getAll() {
         log.info("Retornando todas as tarefas");
         return repository.findAll().stream().map(task -> mapper.toDto(task)).toList();
+    }
+
+    private Task searchForId(Long id) {
+        return repository.findById(id).orElseThrow(() -> {
+            log.warn("ID {} não encontrado no banco de dados", id);
+            return new TaskNotFoundException("ID " + id + " não encontrado no banco de dados");
+        } );
     }
 }
