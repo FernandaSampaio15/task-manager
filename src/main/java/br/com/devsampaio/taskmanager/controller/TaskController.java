dd6feacd7dfd583839aef5fb081a659310824e9e
Fernanda Carolina Sampaio <dev.sampaio7@proton.me>
2026-08-14 13:25:21 -0300

    Estrutura inicial do Controller

--- src/main/java/br/com/devsampaio/taskmanager/controller/TaskController.java
+++ src/main/java/br/com/devsampaio/taskmanager/controller/TaskController.java
@@ -1,4 +1,14 @@
 package br.com.devsampaio.taskmanager.controller;
 
+import br.com.devsampaio.taskmanager.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/tasks")
 public class TaskController {
+
+    private final TaskService service;
 }
