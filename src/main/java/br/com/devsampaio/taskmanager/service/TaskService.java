314016dc1d2c21c8afd6c343ab2d79ca7359a1de
Fernanda Carolina Sampaio <dev.sampaio7@proton.me>
2026-08-14 13:01:22 -0300

    Formatando o código para maior legibilidade

--- src/main/java/br/com/devsampaio/taskmanager/service/TaskService.java
+++ src/main/java/br/com/devsampaio/taskmanager/service/TaskService.java
@@ -24,14 +24,21 @@ public class TaskService {
 
     @Transactional(readOnly = true)
     public List<TaskResponseDto> getAll() {
+
         log.info("Retornando todas as tarefas");
-        return repository.findAll().stream().map(task -> mapper.toDto(task)).toList();
+
+        return repository.findAll()
+                .stream()
+                .map(task -> mapper.toDto(task))
+                .toList();
     }
 
     @Transactional(readOnly = true)
     public TaskResponseDto getById(Long id) {
+
         Task task = searchForId(id);
         log.info("Retornando ID {}", id);
+
         return mapper.toDto(task);
     }
 
@@ -39,7 +46,6 @@ public class TaskService {
     public TaskResponseDto saveTask(TaskRequestDto dto) {
 
         Task task = mapper.toEntity(dto);
-
         log.info("Salvando tarefa de ID {} no banco de dados", task.getId());
 
         return mapper.toDto(task);
@@ -47,9 +53,12 @@ public class TaskService {
 
     @Transactional
     public TaskResponseDto updateTask(Long id, TaskRequestDto dto) {
+
         Task task = searchForId(id);
+
         mapper.updateEntityFromDto(dto, task);
         log.info("Atualizando todos os campos da tarefa de ID {}", id);
+
         return mapper.toDto(task);
     }
 
@@ -59,7 +68,6 @@ public class TaskService {
         Task task = searchForId(id);
 
         mapper.parcialUpdateEntityFromDto(dto, task);
-
         log.info("Atualizando campos específicos da tarefa de ID {}", id);
 
         return mapper.toDto(task);
@@ -74,7 +82,9 @@ public class TaskService {
     }
 
     private Task searchForId(Long id) {
-        return repository.findById(id).orElseThrow(() -> {
+
+        return repository.findById(id)
+                .orElseThrow(() -> {
             log.warn("ID {} não encontrado no banco de dados", id);
             return new TaskNotFoundException("ID " + id + " não encontrado no banco de dados");
         } );
