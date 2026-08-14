f6e9419ada8d50508d2848db4b54a90672a9aeee
Fernanda Carolina Sampaio <dev.sampaio7@proton.me>
2026-08-14 20:03:04 -0300

    Criado o endpoint create no controller

--- src/main/java/br/com/devsampaio/taskmanager/controller/TaskController.java
+++ src/main/java/br/com/devsampaio/taskmanager/controller/TaskController.java
@@ -1,15 +1,14 @@
 package br.com.devsampaio.taskmanager.controller;
 
+import br.com.devsampaio.taskmanager.dto.TaskRequestDto;
 import br.com.devsampaio.taskmanager.dto.TaskResponseDto;
 import br.com.devsampaio.taskmanager.service.TaskService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +27,10 @@ public class TaskController {
     @GetMapping("/{id}")
     public ResponseEntity<TaskResponseDto> getById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(service.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskResponseDto> create(@RequestBody @Valid TaskRequestDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(dto));
     }
 }
