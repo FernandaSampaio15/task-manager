dcbef22e28fd9f74447a8f4ea41a27243d9ac837
Fernanda Carolina Sampaio <dev.sampaio7@proton.me>
2026-08-14 19:59:49 -0300

    Criado o endpoint getById no contrller

--- src/main/java/br/com/devsampaio/taskmanager/controller/TaskController.java
+++ src/main/java/br/com/devsampaio/taskmanager/controller/TaskController.java
@@ -3,9 +3,11 @@ package br.com.devsampaio.taskmanager.controller;
 import br.com.devsampaio.taskmanager.dto.TaskResponseDto;
 import br.com.devsampaio.taskmanager.service.TaskService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,10 @@ public class TaskController {
     @GetMapping
     public ResponseEntity<List<TaskResponseDto>> getAll() {
         return ResponseEntity.status(HttpStatus.OK).body(service.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskResponseDto> getById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.getById(id));
     }
 }
