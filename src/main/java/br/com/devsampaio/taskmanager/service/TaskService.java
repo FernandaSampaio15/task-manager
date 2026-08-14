3f1a715020f123491c7da12d9c614edb9bac6310
Fernanda Carolina Sampaio <dev.sampaio7@proton.me>
2026-08-14 11:05:27 -0300

    Criada a estrutura inicial, injetando dependencias e colocando anotacoes

--- src/main/java/br/com/devsampaio/taskmanager/service/TaskService.java
+++ src/main/java/br/com/devsampaio/taskmanager/service/TaskService.java
@@ -1,4 +1,18 @@
 package br.com.devsampaio.taskmanager.service;
 
+import br.com.devsampaio.taskmanager.mapper.TaskMapper;
+import br.com.devsampaio.taskmanager.model.Task;
+import br.com.devsampaio.taskmanager.repository.TaskRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class TaskService {
+
+    private final TaskMapper mapper;
+    private final TaskRepository repository;
 }
