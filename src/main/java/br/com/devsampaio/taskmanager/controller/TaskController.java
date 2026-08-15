6dcdbbd0c722cd482a96b2ae031b17bc4337d6bd
Fernanda Carolina Sampaio <dev.sampaio7@proton.me>
2026-08-15 11:07:39 -0300

    Tirado um import não utilizado

--- src/main/java/br/com/devsampaio/taskmanager/controller/TaskController.java
+++ src/main/java/br/com/devsampaio/taskmanager/controller/TaskController.java
@@ -6,7 +6,6 @@ import br.com.devsampaio.taskmanager.dto.TaskResponseDto;
 import br.com.devsampaio.taskmanager.service.TaskService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
