222006493c78f0360bd57060aef386fd04a849bc
Fernanda Carolina Sampaio <dev.sampaio7@proton.me>
2026-08-15 21:32:07 -0300

    Atualizado o GlobalExceptionHandler para utilizar do novo DTO ErrorResponse

--- src/main/java/br/com/devsampaio/taskmanager/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/com/devsampaio/taskmanager/exceptions/GlobalExceptionHandler.java
@@ -1,15 +1,24 @@
 package br.com.devsampaio.taskmanager.exceptions;
 
+import br.com.devsampaio.taskmanager.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<String> taskNotFound(TaskNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<ErrorResponse> taskNotFound(TaskNotFoundException e) {
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "Resource not found",
+                e.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
